bb5b23e7787a96b37c063e6996e4cc4e7219a4e0
Amar Hussain <99237742+amarhussain01@users.noreply.github.com>
2023-09-19 14:44:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let x;
+let y;
+let z;
+let maximum;
+let minimum;
+
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x = Math.pow(x, 2);
+//x = Math.sqrt(x);
+//x = Math.abs(x);
+//maximum = Math.max(x, y, x);
+//minimum = Math.min(x, y, z);
+x = Math.PI;
+
+console.log(x);
